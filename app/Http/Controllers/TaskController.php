4888f2a1de94be98332ddeba84e659daabf43d41
Tahmid Ferdous <tahmid.tf1@gmail.com>
2026-04-10 15:50:51 +0600

    routes,controllers and migration file added

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return view('tasks.index');
+    }
+
+    public function getTasks()
+    {
+        return response()->json(Task::latest()->get());
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+        ]);
+
+        $task = Task::create($request->all());
+
+        return response()->json(['success' => true, 'task' => $task]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+
+        return response()->json(['success' => true]);
+    }
+
+    public function destroy($id)
+    {
+        Task::findOrFail($id)->delete();
+
+        return response()->json(['success' => true]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name'     => 'Tahmid Ferdous',
+            'email'    => 'tahmid.tf1@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
     }
 }
